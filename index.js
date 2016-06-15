d36a0ae9c249287770e5c6d8755dd722088b1712
Thomas Dubosc <repraze@gmail.com>
2016-06-15 17:59:57 +0100

    replaces id keyword by key, using object-assign package to prevent relying on Object.assign()

--- index.js
+++ index.js
@@ -1,50 +1,51 @@
 'use strict';
 
 var cache = require("memory-cache");
+const objectAssign = require('object-assign');
 
 module.exports = function(options){
-	var globalOptions = Object.assign({
+	var globalOptions = objectAssign({
 		time		: 1000*60*5,// default time of 5 minutes
 		autoUpdate	: false,	// should update automaticaly after expire
 		debug		: false		// enable debug
 	}, options);
 
 	return {
-		get : function(id, generator, callback, options){
+		get : function(key, generator, callback, options){
 			if(!isNaN(parseFloat(options))){
 				options = {
 					time : options
 				}
 			}
-			options = Object.asign(globalOptions, options);
+			options = objectAssign(globalOptions, options);
 			var time = time || options.defaultTime;
 
-			var args = cache.get(id);
+			var args = cache.get(key);
 			if(args === null){
 				if(options.debug){
-					console.log("Cache: "+id+" - Getting data, next time in "+options.time);
+					console.log("Cache: "+key+" - Getting data, next time in "+options.time);
 				}
 				if(generator instanceof Function){
 					var cacher = function(){
-						cache.put(id, arguments, options.time);
+						cache.put(key, arguments, options.time);
 						callback.apply(null, arguments);
 					}
 					generator(cacher);
 				}
 			}else{
 				if(options.debug){
-					console.log("Cache: "+id+" - Using cached data");
+					console.log("Cache: "+key+" - Using cached data");
 				}
 				if(callback instanceof Function){
 					callback.apply(null, args);
 				}
 			}
 		},
-		del : function(id){
-			cache.del(id);
+		del : function(key){
+			return cache.del(key);
 		},
 		clear : function(){
-			cache.clear();
+			return cache.clear();
 		}
 	}
 }
